4a00be421838ac5b188375efcde5366ce35826fb
phgoodw <75592933+phgoodw@users.noreply.github.com>
2024-05-29 11:42:24 -0400

    Fixed Ada runtime issue

--- ada/main.adb
+++ ada/main.adb
@@ -1,99 +1,145 @@
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 
-procedure Connect_Four is
-   Board : array (1 .. 6, 1 .. 7) of Character := (others => (others => '.'));
-   Current_Player : Character := 'R';
+procedure Main is
+   Board_Size : constant Integer := 6;
+   Column_Size : constant Integer := 7;
+   type Board_Type is array (1 .. Board_Size, 1 .. Column_Size) of Character;
+   Board : Board_Type := (others => (others => ' '));
 
-   procedure Print_Board is
+   Player1 : constant Character := 'X';
+   Player2 : constant Character := 'O';
+   Empty : constant Character := ' ';
+
+   procedure Initialize_Board is
    begin
-      for I in 1 .. 6 loop
-         for J in 1 .. 7 loop
-            Put(Board(I, J));
-            Put(' ');
+      for I in 1 .. Board_Size loop
+         for J in 1 .. Column_Size loop
+            Board(I, J) := Empty;
+         end loop;
+      end loop;
+   end Initialize_Board;
+
+   procedure Display_Board is
+   begin
+      for I in 1 .. Board_Size loop
+         for J in 1 .. Column_Size loop
+            Put(Board(I, J) & " ");
          end loop;
          New_Line;
       end loop;
-   end Print_Board;
+      for J in 1 .. Column_Size loop
+         Put(Integer'Image(J) & " ");
+      end loop;
+      New_Line;
+   end Display_Board;
 
-   function Drop_Disc(Col : Integer) return Boolean is
+   function Is_Column_Full(Column : Integer) return Boolean is
    begin
-      for I in reverse 1 .. 6 loop
-         if Board(I, Col) = '.' then
-            Board(I, Col) := Current_Player;
-            return True;
+      return Board(1, Column) /= Empty;
+   end Is_Column_Full;
+
+   function Get_Column return Integer is
+      Column : Integer;
+   begin
+      loop
+         Put("Enter column (1-7): ");
+         Get(Column);
+         if Column in 1 .. Column_Size and then not Is_Column_Full(Column) then
+            return Column;
+         else
+            Put_Line("Invalid column. Try again.");
          end if;
       end loop;
-      return False;
-   end Drop_Disc;
+   end Get_Column;
 
-   function Check_Direction(Row, Col, Row_Dir, Col_Dir : Integer) return Boolean is
-      Count : Integer := 0;
+   procedure Drop_Token(Column : Integer; Token : Character) is
    begin
-      for I in -3 .. 3 loop
+      for I in reverse 1 .. Board_Size loop
+         if Board(I, Column) = Empty then
+            Board(I, Column) := Token;
+            exit;
+         end if;
+      end loop;
+   end Drop_Token;
+
+   function Check_Win(Token : Character) return Boolean is
+   begin
+      -- Check horizontal win
+      for I in 1 .. Board_Size loop
+         for J in 1 .. Column_Size - 3 loop
+            if Board(I, J) = Token and then Board(I, J + 1) = Token and then
+               Board(I, J + 2) = Token and then Board(I, J + 3) = Token then
+               return True;
+            end if;
+         end loop;
+      end loop;
+
+      -- Check vertical win
+      for I in 1 .. Board_Size - 3 loop
+         for J in 1 .. Column_Size loop
+            if Board(I, J) = Token and then Board(I + 1, J) = Token and then
+               Board(I + 2, J) = Token and then Board(I + 3, J) = Token then
+               return True;
+            end if;
+         end loop;
+      end loop;
+
+      -- Check diagonal win (bottom-left to top-right)
+      for I in 4 .. Board_Size loop
+         for J in 1 .. Column_Size - 3 loop
+            if Board(I, J) = Token and then Board(I - 1, J + 1) = Token and then
+               Board(I - 2, J + 2) = Token and then Board(I - 3, J + 3) = Token then
+               return True;
+            end if;
+         end loop;
+      end loop;
+
+      -- Check diagonal win (top-left to bottom-right)
+      for I in 1 .. Board_Size - 3 loop
+         for J in 1 .. Column_Size - 3 loop
+            if Board(I, J) = Token and then Board(I + 1, J + 1) = Token and then
+               Board(I + 2, J + 2) = Token and then Board(I + 3, J + 3) = Token then
+               return True;
+            end if;
+         end loop;
+      end loop;
+
+      return False;
+   end Check_Win;
+
+   procedure Play_Game is
+      Current_Player : Character := Player1;
+      Moves : Integer := 0;
+      Max_Moves : constant Integer := Board_Size * Column_Size;
+   begin
+      Initialize_Board;
+      loop
+         Display_Board;
+         Put_Line("Player " & Current_Player & "'s turn.");
          declare
-            R : Integer := Row + I * Row_Dir;
-            C : Integer := Col + I * Col_Dir;
+            Column : Integer := Get_Column;
          begin
-            if R >= 1 and R <= 6 and C >= 1 and C <= 7 and Board(R, C) = Current_Player then
-               Count := Count + 1;
-               if Count = 4 then
-                  return True;
-               end if;
+            Drop_Token(Column, Current_Player);
+            Moves := Moves + 1;
+            if Check_Win(Current_Player) then
+               Display_Board;
+               Put_Line("Player " & Current_Player & " wins!");
+               exit;
+            elsif Moves = Max_Moves then
+               Display_Board;
+               Put_Line("It's a draw!");
+               exit;
+            end if;
+            if Current_Player = Player1 then
+               Current_Player := Player2;
             else
-               Count := 0;
+               Current_Player := Player1;
             end if;
          end;
       end loop;
-      return False;
-   end Check_Direction;
-
-   function Is_Winning_Move(Row, Col : Integer) return Boolean is
-   begin
-      return Check_Direction(Row, Col, 1, 0) or -- Horizontal
-             Check_Direction(Row, Col, 0, 1) or -- Vertical
-             Check_Direction(Row, Col, 1, 1) or -- Diagonal \
-             Check_Direction(Row, Col, 1, -1);  -- Diagonal /
-   end Is_Winning_Move;
-
-   procedure Switch_Player is
-   begin
-      if Current_Player = 'R' then
-         Current_Player := 'Y';
-      else
-         Current_Player := 'R';
-      end if;
-   end Switch_Player;
+   end Play_Game;
 
 begin
-   loop
-      Print_Board;
-      Put("Player ");
-      Put(Current_Player);
-      Put_Line(", choose a column (1-7): ");
-      declare
-         Col : Integer;
-      begin
-         Get(Col);
-         if Col < 1 or else Col > 7 or else not Drop_Disc(Col) then
-            Put_Line("Invalid move. Try again.");
-            next;
-         end if;
-      end;
-      declare
-         Row : Integer := 1;
-      begin
-         while Board(Row, Col) /= Current_Player loop
-            Row := Row + 1;
-         end loop;
-         if Is_Winning_Move(Row, Col) then
-            Print_Board;
-            Put("Player ");
-            Put(Current_Player);
-            Put_Line(" wins!");
-            exit;
-         end if;
-      end;
-      Switch_Player;
-   end loop;
-end Connect_Four;
+   Play_Game;
+end Main;
